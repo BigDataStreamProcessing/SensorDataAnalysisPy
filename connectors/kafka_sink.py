4e605a80c4a93d49f51efe3500c7a46481a276a1
kjankiewicz <jankiewicz.krzysztof@gmail.com>
2025-05-07 12:09:13 +0200

    uzupełnienia

--- connectors/kafka_sink.py
+++ connectors/kafka_sink.py
@@ -12,8 +12,8 @@ def get_kafka_sink(properties: dict) -> KafkaSink:
                 .set_value_serialization_schema(SimpleStringSchema())
                 .build()
         ) \
+        .set_transactional_id_prefix("Flink") \
         .set_delivery_guarantee(DeliveryGuarantee.AT_LEAST_ONCE) \
         .build()
 
     return kafka_sink
-
